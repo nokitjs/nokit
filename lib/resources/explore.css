926d07dc6daf327c7422e85e9bb6ee1c9de77d35
Houfeng <admin@xhou.net>
2015-10-31 17:23:17 +0800

    重构

--- lib/resources/explore.css
+++ lib/resources/explore.css
@@ -1,6 +1,6 @@
 /**
- * nokit.js - A Web development framework
- * @version v1.12.0
+ * Nokit.js - A Web development framework
+ * @version v1.12.1
  * @link http://nokit.org
  * @license MIT
  * @author Houfeng
