0a5fe43ab355dcc58ab27bb6ec09e2256482fdcd
Houfeng <admin@xhou.net>
2015-10-31 15:44:17 +0800

    增加应用模板中 global.js 注释

--- examples/nsp/global.js
+++ examples/nsp/global.js
@@ -1,29 +1,53 @@
-var Global = module.exports = function() {};
+/**
+ * 全局应用程序类
+ **/
+var Global = module.exports = function () { };
 
-Global.prototype.onStart = function(server, done) {
+/**
+ * 在应用启动时
+ **/
+Global.prototype.onStart = function (server, done) {
     done();
 };
 
-Global.prototype.onStop = function(server, done) {
+/**
+ * 在应用停止时
+ **/
+Global.prototype.onStop = function (server, done) {
     done();
 };
 
-Global.prototype.onError = function(context, done) {
+/**
+ * 在应用程序出错时
+ **/
+Global.prototype.onError = function (context, done) {
     done();
 };
 
-Global.prototype.onRequestBegin = function(context, done) {
+/**
+ * 在请求开始时
+ **/
+Global.prototype.onRequestBegin = function (context, done) {
     done();
 };
 
-Global.prototype.onReceived = function(context, done) {
+/**
+ * 在请求结束时
+ **/
+Global.prototype.onRequestEnd = function (context, done) {
     done();
 };
 
-Global.prototype.onResponse = function(context, done) {
+/**
+ * 在收到请求数据时
+ **/
+Global.prototype.onReceived = function (context, done) {
     done();
 };
 
-Global.prototype.onRequestEnd = function(context, done) {
+/**
+ * 在响应用时
+ **/
+Global.prototype.onResponse = function (context, done) {
     done();
 };
