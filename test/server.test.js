a18564fd1a26b521332dd7fc4cd3aade621f25ca
Houfeng <admin@xhou.net>
2016-01-20 01:48:09 +0800

    1.17.8

--- test/server.test.js
+++ test/server.test.js
@@ -3,12 +3,14 @@
 var assert = require("assert");
 var app = require("./app");
 
-it("启动后停止", function (done) {
-    app.server.start(function (err, info) {
-        assert.equal(err, null);
-        app.server.stop(function (err, info) {
+describe('Server', function () {
+    it("启动后停止", function (done) {
+        app.server.start(function (err, info) {
             assert.equal(err, null);
-            done();
+            app.server.stop(function (err, info) {
+                assert.equal(err, null);
+                done();
+            });
         });
     });
 });
