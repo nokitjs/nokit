d6bdbf61b99c9ef192e1401e6a902d1b090a6329
Houfeng <admin@xhou.net>
2016-01-20 23:05:00 +0800

    1.17.9 1. 允许作为 express 中间件使用 2. 添加 test case 3. 增加针对 Server 实例的扩展插件机制

---
+++ test/app/plugins/test.js
@@ -0,0 +1,7 @@
+function TestPlugin() { }
+
+TestPlugin.prototype.init = function (server) {
+    server.testPlugin = true;
+};
+
+module.exports = TestPlugin;
---
+++ test/plugin.test.js
@@ -0,0 +1,15 @@
+/* global it */
+/* global describe */
+var assert = require("assert");
+var app = require("./app");
+
+describe('Server plugin', function () {
+    it('use plugin', function (done) {
+        assert.notEqual(app.server.plugins["test"], null);
+        done();
+    });
+    it('init plugin', function (done) {
+        assert.equal(app.server.testPlugin, true);
+        done();
+    });
+});
