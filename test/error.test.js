4ceff782f1114aa7119bd831cdf9166918e60463
Houfeng <admin@xhou.net>
2016-02-01 22:24:52 +0800

    1.19.0 1. 优化请求上下文件 (context) 2. context.send 支持 json 数据 3. 改进 server, 增加进程退出日志 4. 修复一个 http2https 导致的日志记录 Bug 5. 简化日志逻辑 6. 优化进程遇到异常时的退出处理 7. 修复一个 Content-Encoding 的 Bug 8. 处理一个 decodeURI 引起的异常

---
+++ test/error.test.js
@@ -0,0 +1,13 @@
+/* global it */
+/* global describe */
+var assert = require("assert");
+var app = require("./app");
+var request = require('supertest');
+
+describe('Error', function () {
+    it('GET /emitError', function (done) {
+        request(app.server.httpServer)
+            .get("/emitError")
+            .expect(500, done);
+    });
+});
