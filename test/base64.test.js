28b546581584378a15fc54251d33be625b3461db
Houfeng <admin@xhou.net>
2016-02-27 19:52:49 +0800

    1.19.8 1. 更新 cli 参数处理 2. 补充 test case 3. 更新依赖

---
+++ test/base64.test.js
@@ -0,0 +1,22 @@
+/* global nokit */
+/* global it */
+/* global describe */
+var assert = require("assert");
+var app = require("./app");
+var base64 = nokit.base64;
+
+describe('base64', function () {
+
+    it('base64.encode', function () {
+        assert.notEqual(base64.encode, null);
+        var rs = base64.encode("test");
+        assert.equal(rs, "dGVzdA==");
+    });
+
+    it('base64.decode', function () {
+        assert.notEqual(base64.encode, null);
+        var rs = base64.decode("dGVzdA==");
+        assert.equal(rs, "test");
+    });
+
+});
