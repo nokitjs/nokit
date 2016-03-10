dab4e0c6a3895569838bd173e8353d22507335ac
Houfeng <admin@xhou.net>
2016-03-10 22:53:48 +0800

    1.21.0-alpha.4
    1. 重构 Router
    2. 重构 Context
    3. 重构 Response
    4. 重构 Request
    5. 重构 Cookie
    6. 重构 Session
    7. 重构 Logger
    8. 新增 context.buffer
    9. 新增 context.data/context.param

--- test/app/controllers/home.js
+++ test/app/controllers/home.js
@@ -1,106 +1,111 @@
 /**
  * 定义 HomeController
  **/
-var HomeController = module.exports = function () { };
+var HomeController = module.exports = function() { };
 
 /**
  * indexAction
  **/
-HomeController.prototype.index = function () {
+HomeController.prototype.index = function() {
     var self = this;
     self.render("home", {
         "name": "MVC"
     });
 };
 
-HomeController.prototype.say = function () {
+HomeController.prototype.say = function() {
     var self = this;
     self.context.send(self.context.param("name"));
 };
 
-HomeController.prototype.readAndWriteSession = function () {
+HomeController.prototype.readAndWriteSession = function() {
     var self = this;
     var srcVal = parseInt(self.context.param("val"));
-    self.context.session.set("s", srcVal, function () {
-        self.context.session.get("s", function (val) {
+    self.context.session.set("s", srcVal, function() {
+        self.context.session.get("s", function(val) {
             var dstVal = val + val;
             self.context.send(dstVal);
         });
     });
 };
 
-HomeController.prototype.status = function () {
+HomeController.prototype.status = function() {
     var self = this;
     var statusCode = self.context.params["code"];
     self.context.status(statusCode);
 };
 
-HomeController.prototype.statusWithContent = function () {
+HomeController.prototype.statusWithContent = function() {
     var self = this;
     var statusCode = self.context.params["code"];
     self.context.statusWithContent(statusCode);
 };
 
-HomeController.prototype.localeAction = function () {
+HomeController.prototype.localeAction = function() {
     var self = this;
     self.render('locale');
 };
 
-HomeController.prototype.json = function () {
+HomeController.prototype.json = function() {
     var self = this;
     self.context.json('json');
 };
 
-HomeController.prototype.jsonp = function () {
+HomeController.prototype.jsonp = function() {
     var self = this;
     self.context.jsonp('jsonp');
 };
 
-HomeController.prototype.redirect = function () {
+HomeController.prototype.redirect = function() {
     var self = this;
     self.context.redirect('/');
 };
 
-HomeController.prototype.permanentRedirect = function () {
+HomeController.prototype.permanentRedirect = function() {
     var self = this;
     self.context.permanentRedirect('/');
 };
 
-HomeController.prototype.notFound = function () {
+HomeController.prototype.notFound = function() {
     var self = this;
     self.context.notFound();
 };
 
-HomeController.prototype.forbidden = function () {
+HomeController.prototype.forbidden = function() {
     var self = this;
     self.context.forbidden();
 };
 
-HomeController.prototype.notAllowed = function () {
+HomeController.prototype.notAllowed = function() {
     var self = this;
     self.context.notAllowed();
 };
 
-HomeController.prototype.transfer = function () {
+HomeController.prototype.transfer = function() {
     var self = this;
     self.context.transfer('/json');
 };
 
-HomeController.prototype.noChange = function () {
+HomeController.prototype.noChange = function() {
     var self = this;
     self.context.noChange();
 };
 
-HomeController.prototype.emitError = function () {
+HomeController.prototype.emitError = function() {
     var self = this;
     self.__no_func();
 };
 
-HomeController.prototype.cookie = function () {
+HomeController.prototype.cookie = function() {
     var self = this;
     var cookieValue = self.context.cookie.get("test");
     self.context.cookie.set("test", "test");
     self.context.cookie.remove("test");
     self.context.cookie.set("test", "test");
     self.context.send(cookieValue, "text/html");
+};
+
+HomeController.prototype.buffer = function() {
+    var self = this;
+    self.context.buffer(new Buffer("test"), "text/html");
 };
