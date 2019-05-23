13640bfb805cba907d71f34333207bfe67fa103b
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2019-05-23 14:59:27 +0800

    更新 example

--- src/_example/src/app.ts
+++ src/_example/src/app.ts
@@ -1,6 +1,6 @@
 import { Application } from "../..";
 
-new Application({ port: 8080 })
+new Application()
   .launch()
   .then(({ port }) => console.info("Running:", `http://localhost:${port}`))
   .catch(err => console.error(err));
